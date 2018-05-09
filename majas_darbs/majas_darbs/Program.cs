7a982268fb0b41c494600aeff52293414da4a7c7
burkaans <raimonds.buholts@gmail.com>
2018-05-09 23:32:56 +0300

    1. commit
    maajas darbs

---
+++ majas_darbs/majas_darbs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace majas_darbs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ievadi banknotes lielumu, un uzzini, kas atteelots uz dolaara!");
+
+            Console.ReadLine();
+        }
+    }
+}
